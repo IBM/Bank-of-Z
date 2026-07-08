42c90b3b45001ce7fd10a27e750f98ad6d3e6a0d
Jean-Yves BAUDY <baudy.jy@fr.ibm.com>
2026-07-08 18:25:41 +0200

    Update job submission and cancellation for APP_IMS_HLQ
    Signed-off-by: Jean-Yves BAUDY <baudy.jy@fr.ibm.com>

--- .setup/setup-common.sh
+++ .setup/setup-common.sh
@@ -24,12 +24,13 @@ source "$SCRIPTS_DIR/config/setenv.sh"
 #########################################################
 stage_stop_tasks() {
     set +e
+    print_stage "STAGE: Stop running tasks (if any)"
     # =========================
     # Stop IBM IMS regions
     # =========================
-    jsub "${BOZ_IMS_HLQ}.JOBS(STOPMPP1)"  2>/dev/null
-    jsub "${BOZ_IMS_HLQ}.JOBS(STOPMPP2)"  2>/dev/null
-    jsub "${BOZ_IMS_HLQ}.IMSJAVA.JOBS(STOPJMP)"  2>/dev/null
+    jsub "${APP_IMS_HLQ}.JOBS(STOPMPP1)"  2>/dev/null
+    jsub "${APP_IMS_HLQ}.JOBS(STOPMPP2)"  2>/dev/null
+    jsub "${APP_IMS_HLQ}.IMSJAVA.JOBS(STOPJMP)"  2>/dev/null
     sleep 5
     jcan P "IMS2JMP1" 2>/dev/null
     jcan P "IMS2MPP1" 2>/dev/null
@@ -44,8 +45,8 @@ stage_stop_tasks() {
     # =========================
     # Stop IBM zconn servers
     # =========================
-    jcan P "BAQ${APP_NAME}"  2>/dev/null
-    jcan P  "FE${APP_NAME}"  2>/dev/null
+    jcan P "BAQ${APP_BASE_NAME}"  2>/dev/null
+    jcan P  "FE${APP_BASE_NAME}"  2>/dev/null
     
     # =========================
     # Stop IMS1

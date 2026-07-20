2ea319b49b6f37f2ab8fa54f6eb4cda7fce0fb72
Qihan Qiu <qihanqiu@ibm.com>
2026-07-20 10:54:55 -0400

    fix: call addcert.sh without bash prefix to respect its #!/bin/sh shebang
    Signed-off-by: Qihan Qiu <qihanqiu@ibm.com>

--- .setup/setup-common.sh
+++ .setup/setup-common.sh
@@ -334,11 +334,11 @@ stage_setup_certificates() {
     fi
 
     print_info "Running Bank of Z certificate setup script..."
-    print_info "Executing: bash $BANK_DIR/.setup/setup/addcert.sh"
+    print_info "Executing: $BANK_DIR/.setup/setup/addcert.sh"
     cd "$BANK_DIR"
 
     set -o pipefail
-    if bash .setup/setup/addcert.sh; then
+    if .setup/setup/addcert.sh; then
         print_success "RACF keyring and certificates created successfully"
     else
         print_error "Failed to setup RACF certificates"

548f329515d79f7b02606dc029faeecd6f4e20ff
Jean-Yves BAUDY <baudy.jy@fr.ibm.com>
2026-07-23 18:16:06 +0200

    Fix for for when user is not an admin user
    Signed-off-by: Jean-Yves BAUDY <baudy.jy@fr.ibm.com>

--- .setup/setup/grant-db2-user.sh
+++ .setup/setup/grant-db2-user.sh
@@ -41,6 +41,8 @@ rm -f /tmp/Db2-* 2>/dev/null || true
 # =========================
 set +e
 tsocmd "RDEFINE DSNR (${DB2_SSID}.BATCH) UACC(NONE)"
+tsocmd "PERMIT ${DB2_SSID}.BATCH CLASS(DSNR) ID($MYUSER) ACCESS(READ)"
+tsocmd "PERMIT ${DB2_SSID}.BATCH CLASS(DSNR) ID($ZOS_ADMIN_USER) ACCESS(READ)"
 tsocmd "PERMIT ${DB2_SSID}.* CLASS(DSNR) ID($MYUSER) ACCESS(READ)"
 tsocmd "PERMIT ${DB2_SSID}.* CLASS(DSNR) ID($ZOS_ADMIN_USER) ACCESS(READ)"
 tsocmd "SETROPTS RACLIST(DSNR) REFRESH"
@@ -56,6 +58,7 @@ run_job_and_wait "/tmp/CICS-Db2-grant-$$.jcl"
 RC=$?
 
 rm -f /tmp/Db2-*
+rm -f "$SCRIPTS_DIR/../config/.env"
 
 # =========================
 # Result

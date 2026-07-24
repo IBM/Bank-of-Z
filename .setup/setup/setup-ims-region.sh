86012a425602af0d06f57ae2f25127f5822d5c78
Andrew Nguyen <andy.nguyen@ibm.com>
2026-07-24 16:12:05 -0700

    updates for Debug support
    Signed-off-by: Andrew Nguyen <andy.nguyen@ibm.com>

--- .setup/setup/setup-ims-region.sh
+++ .setup/setup/setup-ims-region.sh
@@ -131,7 +131,7 @@ print_info "Setting IMS user to ${IMS_USER} (USS: ${IMS_USER_LOWER})"
 
 zconfig apply -e ims_user="${IMS_USER}" -e ims_user_lower="${IMS_USER_LOWER}"\
               -e imsid="${IMS_DATASTORE}" -e ims_hlq="${IMS_APP_HLQ}" \
-              -e ims_plex="${IMS_PLEX}" \
+              -e ims_plex="${IMS_PLEX}" -e debug_hlq="${DEBUG_HLQ}" \
               -e ims_sys_hlq="${IMS_SYS_HLQ}" -e db2_hlq="${DB2_HLQ}" \
               -e java_home="${JAVA_HOME}" -e db2_java_home="${DB2_JAVA_HOME}" \
               -e ims_java_home="${IMS_JAVA_HOME}" \

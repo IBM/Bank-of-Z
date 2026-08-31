7430b737215a706673c0488fcd53294e434b0d2e
SHIRLEY XIAO <xiaoshir@us.ibm.com>
2026-08-31 00:32:32 -0700

    Remove unsupported IMS zconfig inputs
    Signed-off-by: SHIRLEY XIAO <xiaoshir@us.ibm.com>

--- .setup/setup/setup-ims-region.sh
+++ .setup/setup/setup-ims-region.sh
@@ -100,13 +100,11 @@ zconfig apply -e ims_user="${IMS_USER}" -e ims_user_lower="${IMS_USER_LOWER}"\
               -e imsid="${IMS_DATASTORE}" -e ims_hlq="${IMS_APP_HLQ}" \
               -e ims_plex="${IMS_PLEX}" \
               -e ims_sys_hlq="${IMS_SYS_HLQ}" \
-              -e db2_hlq="${DB2_HLQ}" \
               -e java_home="${JAVA_HOME}" \
               -e db2_java_dir="${DB2_JAVA_FOLDER}" \
               -e ims_java_dir="${IMS_JAVA_FOLDER}" \
               -e ims_java_home="${IMS_JAVA_HOME}" \
               -e db2_ssid="${DB2_SSID}" \
-              -e debug_hlq="${DEBUG_HLQ}" \
               -e ims_target_user="${IMS_USER}" \
               -e ims_ixvolser="${IMS_IXVOLSER}" \
               ims-region.yaml -v
